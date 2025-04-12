cf19584466a7566a36e9d8b6072feda5a1fb9e78
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-04-12 14:25:56 -0300

    feat: add modules to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MagicItemModule } from './modules/magic-item/magic-item.module';
+import { CharacterModule } from './modules/character/character.module';
+import { EnvironmentModule } from './common/environment/environment.module';
+import { PrismaModule } from './infra/prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [EnvironmentModule, PrismaModule, MagicItemModule, CharacterModule],
   providers: [],
 })
 export class AppModule {}
