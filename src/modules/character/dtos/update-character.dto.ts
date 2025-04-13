68194134aa27e955209992782c57aaf7ad91ec18
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-04-13 13:27:19 -0300

    feat: remove class from character update

--- src/modules/character/dtos/update-character.dto.ts
+++ src/modules/character/dtos/update-character.dto.ts
@@ -1,4 +1,48 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateCharacterDto } from './create-character.dto';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
 
-export class UpdateCharacterDto extends PartialType(CreateCharacterDto) {}
+export class UpdateCharacterDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Matches(/^[a-zA-Z\s]+$/, {
+    message: 'Name must contain only letters',
+  })
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString()
+  nickname: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(100)
+  level: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  strength: number;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Max(10)
+  defense: number;
+}
