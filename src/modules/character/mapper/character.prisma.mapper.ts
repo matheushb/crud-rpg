dc6015c13665aa68c41833565f36ac6296197685
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-04-13 13:27:59 -0300

    feat: add magic items to mapper

--- src/modules/character/mapper/character.prisma.mapper.ts
+++ src/modules/character/mapper/character.prisma.mapper.ts
@@ -1,31 +1,35 @@
 import { Character as PrismaCharacter } from '@prisma/client';
 import { Character } from '../entity/character.entity';
 import { CharacterMapper } from './character.mapper';
+import { MagicItem } from '@/modules/magic-item/entity/magic-item.entity';
 
 export class CharacterPrimaMapper implements CharacterMapper<PrismaCharacter> {
-  mapToEntity(prismaCharacter: PrismaCharacter): Character {
-    return {
+  mapToEntity(
+    prismaCharacter: PrismaCharacter & { magic_items?: MagicItem[] },
+  ): Character {
+    return new Character({
       id: prismaCharacter.id,
       name: prismaCharacter.name,
+      nickname: prismaCharacter.nickname,
       class: prismaCharacter.class,
+      strength: prismaCharacter.strength,
       defense: prismaCharacter.defense,
       level: prismaCharacter.level,
-      nickname: prismaCharacter.nickname,
-      strength: prismaCharacter.strength,
       created_at: prismaCharacter.created_at,
       updated_at: prismaCharacter.updated_at,
-    };
+      magic_items: prismaCharacter.magic_items,
+    });
   }
 
   mapFromEntity(character: Character): PrismaCharacter {
     return {
       id: character.id,
       name: character.name,
+      nickname: character.nickname,
       class: character.class,
+      strength: character.strength,
       defense: character.defense,
       level: character.level,
-      nickname: character.nickname,
-      strength: character.strength,
       created_at: character.created_at,
       updated_at: character.updated_at,
     };
