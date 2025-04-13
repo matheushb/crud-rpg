06d863e2a500341a97c568d202812a68656d50ac
Matheus Baraldi <matheushbaraldi@gmail.com>
2025-04-13 13:28:52 -0300

    feat: add validation on magic-item constructor

--- src/modules/magic-item/entity/magic-item.entity.ts
+++ src/modules/magic-item/entity/magic-item.entity.ts
@@ -1,7 +1,18 @@
+import { UnprocessableEntityException } from '@nestjs/common';
 import { ApiProperty } from '@nestjs/swagger';
-import { MagicItemType } from '../enums/magic-item-type.enum';
-import { EntityProps } from '@/common/types/entity-props.type';
 import { randomUUID } from 'crypto';
+import { MagicItemType } from '../enums/magic-item-type.enum';
+
+interface MagicItemProps {
+  id?: string;
+  name: string;
+  strength: number;
+  defense: number;
+  character_id?: string;
+  type: MagicItemType;
+  created_at?: Date;
+  updated_at?: Date;
+}
 
 export class MagicItem {
   @ApiProperty()
@@ -28,7 +39,27 @@ export class MagicItem {
   @ApiProperty({ example: new Date() })
   updated_at: Date;
 
-  constructor(entity: EntityProps<typeof MagicItem>) {
+  constructor(entity: MagicItemProps) {
+    const { strength, defense, type } = entity;
+
+    if (strength + defense > 10 || strength + defense === 0) {
+      throw new UnprocessableEntityException(
+        'The sum of strength and defense must be between 1 and 10',
+      );
+    }
+
+    if (type === MagicItemType.WEAPON && defense > 0) {
+      throw new UnprocessableEntityException(
+        'MagicItem of type WEAPON cannot have a defense value greater than 0',
+      );
+    }
+
+    if (type === MagicItemType.ARMOUR && strength > 0) {
+      throw new UnprocessableEntityException(
+        'MagicItem of type ARMOUR cannot have a strength value greater than 0',
+      );
+    }
+
     this.id = entity.id ?? randomUUID();
     this.name = entity.name;
     this.strength = entity.strength;
